80e06c211e1a7f629055af9e36d4da314f71aafc
Viderspace <50073459+Viderspace@users.noreply.github.com>
2023-09-01 07:58:30 +0300

    Touch Input Refinement test3

--- JS_Scripts/LettersManager.js
+++ JS_Scripts/LettersManager.js
@@ -56,7 +56,7 @@ let secondLineOffset = ResponsiveSize(container.clientWidth,
 
 let ResponsiveScale = ResponsiveSize(container.clientWidth, 0.4, 0.5, 1.3, 1.6);
 let scale = ResponsiveScale * 0.2;
-var colliderShrinkFactor = 0.95;
+var colliderShrinkFactor = 0.8;
 
 
 let inflateScale = 2
